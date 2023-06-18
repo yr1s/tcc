601b2630815489bb77651d806edd460a8675d78f
leonardo <leonardo_abadie@proton.me>
2023-06-17 23:28:28 -0300

    assets for view pages

---
+++ asyc/src/view/assets/components/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Yris">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/estilo-topo.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+        *{
+            font-family: 'Jost', sans-serif;
+            font-size: 15px;
+        }
+    </style>
+</head>
+<body>
+  <header >
+    <nav class="navbar navbar-dark bg-dark navbar-expand-sm ">
+      <div class="container-fluid"> 
+        <a class="navbar-brand" href="">
+          <img src="assets/img/asyc_texto_versao_dark.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto me-5 pe-5">
+            <li class="nav-item me-2">
+              <a href="../../sectors/service-order/index.php" class="nav-link ">Ordens de serviço</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="asyc-pageCliente.php" class="nav-link ">Clientes</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="asyc-pageEstoque.php" class="nav-link ">Estoque</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="asyc-pageRh.php" class="nav-link ">RH</a>
+            </li>
+            <li class="nav-item me-2 ">
+              <a href="financeiroLancamento.php" class="nav-link ">Finaceiro</a>
+            </li>
+            <li class="nav-item">
+              <li class="nav-item dropdown me-1">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fa-solid fa-user fa-lg" style="color: #ff0000;"></i>
+                </a>
+                <div class="dropdown-menu" >
+                  <a class="dropdown-item" href="#">Sair</a>
+                </div>
+              </li>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
